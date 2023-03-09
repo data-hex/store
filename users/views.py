dde67d7c8b15e23e974bb059bf43bdd799f348b1
Natalia Popova <sverdlova3@gmail.com>
2023-03-09 11:54:24 +0500

    edit context

--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
 # Create your views here.
+
 
 def login(request):
     if request.method == 'POST':
@@ -24,6 +26,7 @@ def login(request):
     context = {'form': form}
     return render(request, 'users/login.html', context)
 
+
 def registration(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
@@ -37,6 +40,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -47,8 +51,13 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form, 'baskets': Basket.objects.filter(user=request.user),}
+
+    context = {'title': 'Store - Профиль',
+               'form': form,
+               'baskets': Basket.objects.filter(user=request.user),
+               }
     return render(request, 'users/profile.html', context)
+
 
 def logout(request):
     auth.logout(request)
