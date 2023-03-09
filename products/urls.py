8a38d7f5ffaad7be69d7c5dd0b4222648b3ccb93
Natalia Popova <sverdlova3@gmail.com>
2023-03-09 18:17:48 +0500

    add path for paginator_category

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', products, name='index'),
     path('category/<int:category_id>/', products, name='category'),
     path('page/<int:page_number>/', products, name='paginator'),
+    path('category/<int:category_id>/<int:page_number>/', products, name='paginator_category'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
