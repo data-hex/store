8aad63b5056d709905e697294d479474ad7da984
Natalia Popova <sverdlova3@gmail.com>
2023-03-13 13:36:35 +0500

    edit send_verification_email

--- users/models.py
+++ users/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
+from django.urls import reverse
+from django.conf import settings
+
+from django.utils.timezone import now
 
 # Create your models here.
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
+    email = models.EmailField(unique=True)
     is_verified_email = models.BooleanField(default=False)
 
 class EmailVerification(models.Model):
@@ -18,11 +23,20 @@ class EmailVerification(models.Model):
 
 
     def send_verification_email(self):
+        link = reverse('users:email_verification', kwargs={'email': self.user.email, 'code': self.code})
+        verification_link = f'{settings.DOMAIN_NAME}{link}'
+        subject = f'Подверждение учетной записи для {self.user.username}'
+        message = 'Для подверждения учетной записи для {} перейдите по ссылке: {}'.format(
+            self.user.email,
+            verification_link
+        )
         send_mail(
-            'Subject here',
-            'Test verification email.',
-            'from@example.com',
-            [self.user.email],
+            subject=subject,
+            message=message,
+            from_email='from@email.com',
+            recipient_list=[self.user.email],
             fail_silently=False,
         )
 
+    def is_expired(self):
+        return True if now() >= self.expiration else False
