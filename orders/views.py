64d61812909b500839a91f464e4024955d95367b
Natalia Popova <sverdlova3@gmail.com>
2023-03-28 11:14:40 +0500

    create OrderListView

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,35 @@
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
-from django.urls import reverse, reverse_lazy
+from django.views.generic.list import ListView
 
 from common.views import TitleMixin
-
 from orders.forms import OrderForm
+from orders.models import Order
 
 # Create your views here.
+
+class OrderListView(TitleMixin, ListView):
+    template_name = 'orders/orders.html'
+    title = 'Store - Заказы'
+    queryset = Order.objects.all()
+    ordering = ('-created')
+
+    def get_queryset(self):
+        queryset = super(OrderListView, self).get_queryset()
+        return queryset.filter(initiator=self.request.user)
+
+
+class OrderDetailView(DetailView):
+    template_name = 'orders/order.html'
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderDetailView, self).get_context_data(**kwargs)
+        context['title'] = f'Store - Заказ # {self.object.id}'
+        return context
+
+
 class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order-create.html'
     form_class = OrderForm
