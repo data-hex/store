0e668f16920acb19a09242a376b3744492da5664
Natalia Popova <sverdlova3@gmail.com>
2023-05-11 15:05:40 +0500

    blank true for image in Product

--- products/models.py
+++ products/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_image')
+    image = models.ImageField(upload_to='products_image', blank=True)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     class Meta:
