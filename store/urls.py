4836a7f5cea2f7dd65250772352a4d51729ee4c1
Natalia Popova <sverdlova3@gmail.com>
2023-05-12 11:25:35 +0500

    flake8

--- store/urls.py
+++ store/urls.py
@@ -18,10 +18,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
+from rest_framework.authtoken.views import obtain_auth_token
 
 from products.views import IndexView
-
-from rest_framework.authtoken.views import obtain_auth_token
 
 static_urlpatterns = [
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
